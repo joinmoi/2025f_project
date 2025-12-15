9646783441c57c5e519dc65cd88407bb38047de5
Mona Lisa <anitaimanbeig@gmail.com>
2025-12-15 10:45:14 -0500

    we initialized our templates folder

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 # project file 
 
-from flask import Flask
+from flask import Flask, render_template
 # Create the Flask app
 app = Flask(__name__)
 
@@ -8,6 +8,10 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return "Welcome to our Flask Web Application!"
+
+@app.route("/about")
+def about(): 
+    return render_template("about.html")
 
 # This allows you to run the app directly
 if __name__ == "__main__":
